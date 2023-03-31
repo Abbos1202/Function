53924c47bd6ef480805744322f268361e8e6b5d2
Abbos Ibrohimov <abbosibrohimov07@gmail.com>
2023-03-31 15:16:47 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,57 @@
+// Math.round
+
+// function Webround(son){
+//     return Number.parseInt(son)
+// }
+
+// console.log(Webround(5.6));
+
+
+// ======================================
+
+// Math.ceil
+
+// function Webceil(son){
+//     return Number.parseInt(++son)
+// }
+// console.log(Webceil(10.001));
+
+// ======================================
+
+// Math.floor
+
+// function Webfloor(son){
+//     return Number.parseInt(son)
+// }
+
+// console.log(Webfloor(15.987));
+
+// ======================================
+
+// Math.abs
+
+// function Webabs(son){
+//     return Math.abs(son)
+// }
+
+// console.log(Webabs(-5));
+
+// ======================================
+
+// Math.pow
+
+// function Webpow(son, daraja) {
+//     return son ** daraja
+// }
+
+// console.log(Webpow(5, 2));
+
+// ======================================
+
+// Math.sqrt
+
+function Websqrt(son) {
+    return son ** 0.5
+}
+
+console.log(Websqrt(25));
